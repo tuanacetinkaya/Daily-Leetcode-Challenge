e0ff0801413389098fd24cdc577a6382ac8114cf
Tuana Çetinkaya <47085047+tuanacetinkaya@users.noreply.github.com>
2021-01-24 02:14:27 +0300

    solution added

--- January 2021/24.01.2021/tuana.py
+++ January 2021/24.01.2021/tuana.py
@@ -1,1 +1,10 @@
-print "boyle yusuf"
+class Solution:
+    def subtractProductAndSum(self, n: int) -> int:
+
+        proDigit, sumDigit = 1 , 0
+        for d in str(n):
+            proDigit *= int(d)
+            sumDigit += int(d)
+            
+        return proDigit - sumDigit
+        
