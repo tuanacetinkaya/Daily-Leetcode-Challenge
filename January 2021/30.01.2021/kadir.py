ea40cf1c02707e5ebd5ca46b1e661d5f3f3061a8
Abdulkadir Alaca <76223197+abdulkadiralaca@users.noreply.github.com>
2021-01-30 01:03:49 +0300

    :)

--- January 2021/30.01.2021/kadir.py
+++ January 2021/30.01.2021/kadir.py
@@ -1,11 +1,10 @@
 class Solution:
     def containsPattern(self, arr: List[int], m: int, k: int) -> bool:
-        a = False
         for i in range(0,len(arr)-m):
             t=1
             for j in range(1,k):
                 if arr[i:i+m] == arr[i+(m*j):i+(m*j)+m]:
                     t+=1
             if t >= k:
-                a = True
-        return a
+                return True
+        return False
