be6c2da71b84a0187ef2ffd8a33c6a319e7124b9
Eyyüp Ensar Topuz <56695631+etopuz@users.noreply.github.com>
2021-01-31 21:30:08 +0300

    Add files via upload

---
+++ January 2021/31.01.2021/Ensar.cs
@@ -0,0 +1,20 @@
+public class Solution {
+    public int CountBattleships(char[][] board) {
+        int numberOfBattleships = 0;
+        for (int i = 0; i < board.Length ; i++){
+            for(int j = 0; j < board[i].Length ; j++){
+                if (board[i][j] == 'X') {
+                    if (i < board.Length-1){
+                        if (board[i+1][j] == 'X')
+                            numberOfBattleships--;
+                    }   
+                    if (j == board[i].Length - 1)
+                        numberOfBattleships++; 
+                    else if (board[i][j+1] == '.')
+                        numberOfBattleships++; 
+                }
+            }
+        }
+        return numberOfBattleships;
+    }
+}
