93d194acb80c77caec385087ba33524e4baaf5b3
Tuana Çetinkaya <47085047+tuanacetinkaya@users.noreply.github.com>
2021-02-13 16:24:11 +0300

    Create tuana.py

---
+++ February 2021/10.02.2021/tuana.py
@@ -0,0 +1,36 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    
+    def findMaxIdx(self, nums):
+        
+        val, idx = -1, -1
+        for i, num in enumerate(nums):
+            if num > val:
+                val = num
+                idx = i
+        return idx
+    
+    
+    def buildTree(self, nums):
+        if not nums:
+            return None
+        if len(nums) == 1:
+            root = TreeNode(nums[0])
+            return root
+        
+        maxIdx = self.findMaxIdx(nums)
+        
+        root = TreeNode(nums[maxIdx])
+        root.left = self.buildTree(nums[:maxIdx])
+        root.right = self.buildTree(nums[maxIdx+1:]  if maxIdx < len(nums) else [])
+        return root
+        
+        
+    def constructMaximumBinaryTree(self, nums: List[int]) -> TreeNode:
+        return self.buildTree(nums)
+    
