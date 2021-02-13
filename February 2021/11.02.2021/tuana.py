6697c6b40203afeb1b27f5a305db5de498a75eed
Tuana Çetinkaya <47085047+tuanacetinkaya@users.noreply.github.com>
2021-02-13 15:50:33 +0300

    Create tuana.py

---
+++ February 2021/11.02.2021/tuana.py
@@ -0,0 +1,9 @@
+class Solution:
+    def transpose(self, matrix: List[List[int]]) -> List[List[int]]:
+        transpose =  [[0 for i in range(len(matrix))] for j in range(len(matrix[0]))]
+        for i in range(len(matrix)):
+            for j in range(len(matrix[0])):
+                transpose[j][i] = matrix[i][j]
+
+        return transpose
+        
