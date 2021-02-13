ea3980ccff9dc4ac5a26fd8ed7f2a955dce56f9f
Tuana Çetinkaya <47085047+tuanacetinkaya@users.noreply.github.com>
2021-02-13 16:29:46 +0300

    let them all speak now:

--- February 2021/12.02.2021/tuana.py
+++ February 2021/12.02.2021/tuana.py
@@ -1,9 +1,3 @@
-# Definition for a binary tree node.
-# class TreeNode:
-#     def __init__(self, val=0, left=None, right=None):
-#         self.val = val
-#         self.left = left
-#         self.right = right
 class Solution:
     def inorderTraversal(self, root: TreeNode) -> List[int]:
         inOrder = []
@@ -19,4 +13,3 @@ class Solution:
                 inOrder.append(temp.val)
                 root = temp.right
         return inOrder
-    
