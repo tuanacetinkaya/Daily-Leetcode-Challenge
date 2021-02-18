9e7057bef778faf35099194e61ae0deae18f6f85
Buğra Ercan <38170967+sanshigo345@users.noreply.github.com>
2021-02-18 18:20:50 +0300

    hintlersaolsun

---
+++ February 2021/18.02.2021/sanshigo.cpp
@@ -0,0 +1,19 @@
+class Solution {
+    public int countPrimes(int n) {
+       boolean[] isPrime = new boolean[n];
+       for (int i = 2; i < n; i++) {
+          isPrime[i] = true;
+       }
+       for (int i = 2; i * i < n; i++) {
+          if (!isPrime[i]) continue;
+          for (int j = i * i; j < n; j += i) {
+             isPrime[j] = false;
+          }
+       }
+       int count = 0;
+       for (int i = 2; i < n; i++) {
+          if (isPrime[i]) count++;
+       }
+       return count;
+    }
+}
