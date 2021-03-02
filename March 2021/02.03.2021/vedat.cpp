862bd34a36c40c38794ff2255b18f17e30faaaca
Vedat Baday <54285744+badayvedat@users.noreply.github.com>
2021-03-02 04:50:05 -0800

    Create vedat.cpp

---
+++ March 2021/02.03.2021/vedat.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    vector<int> findErrorNums(vector<int>& nums) {
+        unordered_map<int, int> um;
+        for (auto& n: nums) um[n]++;
+    
+        vector<int> ans;
+        for (auto x: um) {
+            if (x.second == 2) {
+                ans.push_back(x.first);
+            }
+        }
+        for (int i=1; i<=nums.size(); i++) {
+            if (!um.count(i)) {
+                ans.push_back(i);
+                break;
+            }
+        }
+        return ans;
+    }
+};
