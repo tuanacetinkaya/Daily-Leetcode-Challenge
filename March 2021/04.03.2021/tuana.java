c3772e626d8b16d1d78dd2d2278176b873b3322d
Tuana Çetinkaya <47085047+tuanacetinkaya@users.noreply.github.com>
2021-03-06 00:33:40 +0300

    ilkim cozdu ben javaya cevirdim kips bye ^^ :3

--- March 2021/04.03.2021/tuana.java
+++ March 2021/04.03.2021/tuana.java
@@ -1,14 +1,4 @@
-/**
- * Definition for singly-linked list.
- * public class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode(int x) {
- *         val = x;
- *         next = null;
- *     }
- * }
- */
+
 public class Solution {
     public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
         ArrayList<ListNode> nodes = new ArrayList<ListNode>();
