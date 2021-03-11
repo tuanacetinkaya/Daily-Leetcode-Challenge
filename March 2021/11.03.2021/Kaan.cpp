5b79dbedc1f291b4c620d8d5377472a476c5ce35
Kaan Tunçer <54275187+kaantuncer@users.noreply.github.com>
2021-03-11 16:54:22 +0300

    POG

---
+++ March 2021/11.03.2021/Kaan.cpp
@@ -0,0 +1,24 @@
+class Solution {    
+    public int coinChange(int[] coins, int amount) {
+        
+        int[] dpArray = new int[amount+1];
+        Arrays.fill(dpArray, Integer.MAX_VALUE);
+        dpArray[0] = 0;
+        
+        for(int i = 0 ; i<coins.length;i++){
+            for(int j = coins[i] ; j<=amount;j++){
+                if(dpArray[j-coins[i]] != Integer.MAX_VALUE){
+                    dpArray[j] = Math.min(dpArray[j-coins[i]] + 1 , dpArray[j]);    
+                }
+            }
+            
+        }
+        if(dpArray[amount] == Integer.MAX_VALUE){
+            return -1;
+        }
+        return dpArray[amount];
+        
+        
+        
+    }
+}
