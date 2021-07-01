9c62c16070347bd004c1f234f49bed90cb9e52ef
Vedat Baday <54285744+badayvedat@users.noreply.github.com>
2021-07-01 10:40:05 +0300

    tle
    solution from https://xiaoguan.gitbooks.io/leetcode/content/LeetCode/89-gray-code-medium.html

---
+++ July 2021/01.07.2021/vedat.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    vector<int> grayCode(int n) {
+        vector<int> res;
+        for (int i = 0; i < pow(2,n); ++i) {
+            res.push_back((i >> 1) ^ i);
+        }
+        return res;
+    }
+};
