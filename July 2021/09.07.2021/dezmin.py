b5cd0337670188aa0ed5d96d08678fcd0a0f2a35
mavibirdesmi <mavibirdesmi@gmail.com>
2021-07-10 08:59:28 +0300

    geekforgeeksteki nlogn çözüm bayağı iyi, ama benimki n^2 :sadpepe:
    https://www.geeksforgeeks.org/longest-increasing-subsequence-dp-3/

---
+++ July 2021/09.07.2021/dezmin.py
@@ -0,0 +1,10 @@
+class Solution:
+    def lengthOfLIS(self, nums: List[int]) -> int:
+        n = len(nums)
+        dp = [1 for j in range(n)]
+        for i in range(1, n):
+            for j in range(i):
+                if nums[i] > nums[j]:
+                    dp[i] = max(dp[j] + 1, dp[i])
+        
+        return(max(dp))      
